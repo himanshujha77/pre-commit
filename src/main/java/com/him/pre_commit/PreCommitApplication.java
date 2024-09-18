513ecd13f83756adf7ab8bad8969c2d022e29d4d
Himanshu Jha <himanshu.jha.777@gmail.com>
2024-09-18 10:12:02 +0530

    first commit

---
+++ src/main/java/com/him/pre_commit/PreCommitApplication.java
@@ -0,0 +1,14 @@
+package com.him.pre_commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreCommitApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/him/pre_commit/PreCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.him.pre_commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
